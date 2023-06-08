521a3a70395f20a8097da218ca195e97ac989b48
cs2812 <cs786786786@gmail.com>
2023-06-08 20:12:57 +0530

    build

--- build/models/user.model.js
+++ build/models/user.model.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
+exports.UserModel = void 0;
 const mongoose_1 = require("mongoose");
 const userSchema = new mongoose_1.Schema({
-    username: { type: String, require: true },
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
 });
-exports.User = (0, mongoose_1.model)("user", userSchema);
+exports.UserModel = (0, mongoose_1.model)("user", userSchema);
